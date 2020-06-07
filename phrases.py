4d23aaca26c5f3bebd58de68b8ff73c072b0a927
YashIndane <53041219+YashIndane@users.noreply.github.com>
2020-06-07 16:50:49 +0530

    Add files via upload

---
+++ phrases.py
@@ -0,0 +1,20 @@
+P = {
+
+    'total cases' : 1,
+    'new cases'   : 2,
+    'total deaths': 3,
+    'new deaths'  : 4,
+    'total recovered' : 5,
+    'new recovered' : 6,
+    'active cases' : 7,
+    'critical' : 8,
+    'total cases per million population' : 9,
+    'deaths per million population' : 10,
+    'total tests' : 11,
+    'tests per million population' : 12,
+    'population' : 13,
+    'one case every x ppl' : 14,
+    'one death every x ppl': 15,
+    'one test every x ppl' :16
+    
+    }
